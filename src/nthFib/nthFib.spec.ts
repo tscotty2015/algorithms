44bbc1d4f25747d865a7baddb9acc53511b1512e
badassbison <shawn.scott.xd@gmail.com>
2022-05-30 16:25:58 -0500

    Added nthFib

---
+++ src/nthFib/nthFib.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { nthFib } from './nthFib';
+
+describe('isPrime', () => {
+  it('should return true for 2', () => {
+    expect(nthFib(1)).equal(1);
+  });
+});
