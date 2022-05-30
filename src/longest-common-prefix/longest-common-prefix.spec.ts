ad179f9efa120cc9d32174ef8b8a8511a7bc2eea
tscotty2015 <tscotty2015@gmail.com>
2022-05-30 16:33:45 -0500

    practice

---
+++ src/longest-common-prefix/longest-common-prefix.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { longestCommonPrefix } from './longest-common-prefix';
+
+describe('longestCommonPrefix', () => {
+    it('should return 1', () => {
+        // expect(longestCommonPrefix()).equal(1);
+    });
+});
---
+++ src/longest-common-prefix/longest-common-prefix.ts
@@ -0,0 +1,3 @@
+export const longestCommonPrefix = (strs: string[]): string => {
+    return;
+};
