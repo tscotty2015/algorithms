88ae285a3a1474b4ad792776a224d1c501e7af26
badassbison <shawn.scott.xd@gmail.com>
2022-05-30 16:30:13 -0500

    Add comments

--- src/nthFib/nthFib.ts
+++ src/nthFib/nthFib.ts
@@ -1,3 +1,13 @@
+// fib sequence 1 1 2 3 5 8 13 ...
+// next is last 2 added together
+// when given n, return that number at that place
+// Example n = 1, returns 1
+// Example n = 2, returns 1
+// Example n = 3, returns 2
+// Example n = 4, returns 3
+// Example n = 5, returns 5
+// Example n = 6, returns 8
+
 export const nthFib = (n: number): number => {
   return;
 }
